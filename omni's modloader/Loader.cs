4330ecad7ebcd1949c9a8ac1394a3da12e1bbdc9
Cooper <cooper@stevetex.com>
2015-01-16 09:09:47 -0800

    loader base

---
+++ omni's modloader/Loader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace omni_s_modloader
+{
+    public class Loader
+    {
+        public bool init()
+        {
+            if (!Directory.Exists("Plugins"))
+                Directory.CreateDirectory("Plugins");
+            return true;
+        }
+    }
+}
--- omni's modloader/Program.cs
+++ omni's modloader/Program.cs
@@ -8,10 +8,19 @@ namespace omni_s_modloader
 {
     class Program
     {
+        public const string version = "v1.0";
+
         static void Main(string[] args)
         {
+            Console.Title = "omni's modloader - " + version;
+            Loader l = new Loader();
+            Console.Write("[INFO]Initializing omni's modloader {0}...", version);
+            bool result = l.init();
+            Console.Write(result ? " success" : "\r\n[ERROR]Failed. Exiting.");
+            if (!result)
+                return;
             Injector i = new Injector();
-            Console.WriteLine(i.Inject("isaac-ng"));
+            Console.WriteLine("\r\n\r\n[INFO]Injection result: " + i.Inject("isaac-ng"));
             Console.ReadKey();
         }
     }
